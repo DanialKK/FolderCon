b70f5efab6eee0090ca7e1ab5570e7317d37e737
Danial <danial.khoshkhan@gmail.com>
2025-06-08 13:27:24 +0330

    Django Admin Customization

--- Foldercon/master/admin.py
+++ Foldercon/master/admin.py
@@ -1,3 +1,36 @@
+from django.contrib.admin import AdminSite
 from django.contrib import admin
+from .models import Type, Category, FolderCon
 
-# Register your models here.
+class MyAdminSite(AdminSite):
+    site_header = "FolderCon Admin"
+    site_title = "FolderCon"
+    index_title = "Welcome to FolderCon Admin"
+
+    def get_app_list(self, request, app_label=None):
+        app_list = super().get_app_list(request)
+        for app in app_list:
+            app['models'].sort(key=lambda x: ['Type', 'Category', 'FolderCon'].index(x['object_name']))
+        return app_list
+
+admin_site = MyAdminSite(name='myadmin')
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'ico')
+    search_fields = ('title',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'slug')
+    list_filter = ('type',)
+    search_fields = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+class FolderContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'ico', 'png', 'icns', 'zip', 'webp')
+    list_filter = ('category',)
+    search_fields = ('title',)
+
+# ثبت مدل‌ها با ادمین سفارشی
+admin_site.register(Type, TypeAdmin)
+admin_site.register(Category, CategoryAdmin)
+admin_site.register(FolderCon, FolderContentAdmin)
